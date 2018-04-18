00cd74e7874f4df593e5416e1f7e560713cc179a
frinkiac-7.net <rob@frinkiac-7.net>
2018-04-18 18:25:24 -0400

    Commit post-merge conflict   * Experienced merge conflict during last commit   * Attempted to resolve using github docsbut     - Missed a step from the docs and db/schema wasn't included   * Not sure if this will work as expected

--- db/schema.rb
+++ db/schema.rb
@@ -65,11 +65,7 @@ ActiveRecord::Schema.define(version: 20180418092907) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-    t.boolean "isadmin"
-=======
     t.boolean "isadmin", default: false
->>>>>>> client-ctrlr
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["token"], name: "index_users_on_token", unique: true
   end
