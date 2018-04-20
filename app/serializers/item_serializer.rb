32424d6708814895423c77d1f23b4a5f7ac5bac4
frinkiac-7.net <rob@frinkiac-7.net>
2018-04-20 06:03:46 -0400

    Add editable attribute to item serializer   * Required for next stage of development   * Added to enable testing during development

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :category, :user_id
+  attributes :id, :name, :description, :price, :category, :user_id, :editable
+
+  def editable
+    scope == object.user
+  end
 end
